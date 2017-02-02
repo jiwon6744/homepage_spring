da5db54575fd1648423528f62c4c49ca07c2e1f2
kimjiwon <skywldnjs21@naver.com>
2017-02-02 11:12:31 +0900

    list total add

--- spring_blog/src/main/java/spring/model/memo/MemoDAO.java
+++ spring_blog/src/main/java/spring/model/memo/MemoDAO.java
@@ -5,9 +5,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import spring.utility.blog.DBClose;
@@ -15,33 +18,13 @@ import spring.utility.blog.DBOpen;
 
 @Repository
 public class MemoDAO {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
 	public int total(String col, String word) {
-		int total = 0;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT COUNT(*) ");
-		sql.append(" FROM memo ");
-		if (word.trim().length() > 0)
-			sql.append(" WHERE " + col + " LIKE '%'||?||'%' ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			if(word.trim().length() > 0)
-				pstmt.setString(1, word);
-			
-			rs = pstmt.executeQuery();
-			if(rs.next()){
-				total = rs.getInt(1);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			DBClose.close(con, pstmt, rs);
-		}
-		
-		return total;
+		Map map = new HashMap();
+		map.put("col", col);
+		map.put("word", word);
+		return sqlSession.selectOne("memo.total", map);
 	}
 
 	public void upViewcnt(int memono) {
@@ -177,56 +160,8 @@ public class MemoDAO {
 	}
 
 	public List<MemoDTO> list(Map map) {
-		List<MemoDTO> list = new ArrayList<MemoDTO>();
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		String col = (String) map.get("col");
-		String word = (String) map.get("word");
-		int sno = (Integer) map.get("sno");
-		int eno = (Integer) map.get("eno");
-
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT memono, title, to_char(wdate, 'yyyy-mm-dd') wdate, viewcnt, r ");
-		sql.append(" FROM(");
-		sql.append(" 	SELECT memono, title, wdate, viewcnt, rownum r");
-		sql.append(" 	FROM(");
-		sql.append(" 		SELECT memono, title, wdate, viewcnt");
-		sql.append(" 		FROM memo ");
-		if (word.trim().length() > 0) {
-			sql.append(" WHERE " + col + " like '%'||?||'%' ");
-		}
-		sql.append(" ORDER BY memono DESC ");
-		sql.append(" 		) ");
-		sql.append(" 	)WHERE r >= ? and r <= ?");
-
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			int i = 0;
-			if (word.trim().length() > 0) {
-				pstmt.setString(++i, word);
-			}
-			pstmt.setInt(++i, sno);
-			pstmt.setInt(++i, eno);
-
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				MemoDTO dto = new MemoDTO();
-				dto.setMemono(rs.getInt("memono"));
-				dto.setTitle(rs.getString("title"));
-				dto.setWdate(rs.getString("wdate"));
-				dto.setViewcnt(rs.getInt("viewcnt"));
-
-				list.add(dto);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt, rs);
-		}
-		return list;
+		
+		return sqlSession.selectList("memo.list", map);
+		
 	}
 }
