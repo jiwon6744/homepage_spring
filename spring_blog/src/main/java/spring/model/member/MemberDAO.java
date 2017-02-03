5ba975f457696c1c03b61a0097474b60898fd262
kimjiwon <skywldnjs21@naver.com>
2017-02-03 16:23:22 +0900

    member finish

--- spring_blog/src/main/java/spring/model/member/MemberDAO.java
+++ spring_blog/src/main/java/spring/model/member/MemberDAO.java
@@ -1,10 +1,5 @@
 package spring.model.member;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,9 +7,6 @@ import java.util.Map;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import spring.utility.blog.DBClose;
-import spring.utility.blog.DBOpen;
 
 @Repository
 public class MemberDAO {
@@ -24,188 +16,38 @@ public class MemberDAO {
 
 	public boolean create(MemberDTO dto) {
 		boolean flag = false;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" INSERT INTO member(id, passwd, mname, tel, email, zipcode, ");
-		sql.append(" address1,address2, job, mdate, fname, grade) ");
-		sql.append(" VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, sysdate, ?, 'H') ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, dto.getId());
-			pstmt.setString(2, dto.getPasswd());
-			pstmt.setString(3, dto.getMname());
-			pstmt.setString(4, dto.getTel());
-			pstmt.setString(5, dto.getEmail());
-			pstmt.setString(6, dto.getZipcode());
-			pstmt.setString(7, dto.getAddress1());
-			pstmt.setString(8, dto.getAddress2());
-			pstmt.setString(9, dto.getJob());
-			pstmt.setString(10, dto.getFname());
-
-			int cnt = pstmt.executeUpdate();
-			if (cnt > 0) {
-				flag = true;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt);
+		int cnt = sqlSession.insert("member.create", dto);
+		if (cnt > 0) {
+			flag = true;
 		}
-
 		return flag;
 	}
 
 	public MemberDTO read(String id) {
-		MemberDTO dto = null;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT id, mname, tel, email, zipcode, address1, address2, job, mdate, fname ");
-		sql.append(" FROM member ");
-		sql.append(" WHERE id = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, id);
-
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				dto = new MemberDTO();
-				dto.setId(rs.getString("id"));
-				dto.setMname(rs.getString("mname"));
-				dto.setTel(rs.getString("tel"));
-				dto.setEmail(rs.getString("email"));
-				dto.setZipcode(rs.getString("zipcode"));
-				dto.setAddress1(rs.getString("address1"));
-				dto.setAddress2(rs.getString("address2"));
-				dto.setJob(rs.getString("job"));
-				dto.setMdate(rs.getString("mdate"));
-				dto.setFname(rs.getString("fname"));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt, rs);
-		}
-		return dto;
+		return sqlSession.selectOne("member.read", id);
 	}
 
 	public List<MemberDTO> list(Map map) {
-		List<MemberDTO> list = new ArrayList<MemberDTO>();
-
-		// 디비연결객체생성
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		// map 에서 데이터를 빼온다.
-		String col = (String) map.get("col");
-		String word = (String) map.get("word");
-		int sno = (Integer) map.get("sno");
-		int eno = (Integer) map.get("eno");
-
-		// 쿼리문작성
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT id, mname, tel, email, zipcode, address1, address2, fname, r ");
-		sql.append(" FROM( ");
-		sql.append(" 	SELECT id, mname, tel, email, zipcode, address1, address2, fname, rownum r ");
-		sql.append(" 	FROM( ");
-		sql.append(" 		SELECT id, mname, tel, email, zipcode, address1, address2, fname ");
-		sql.append(" 		FROM member ");
-		if (word.trim().length() > 0)
-			sql.append(" 		WHERE " + col + " LIKE '%'||?||'%' ");
-		sql.append(" 		ORDER BY id desc ");
-		sql.append(" 	 )");
-		sql.append(" )WHERE r>=? and r<=? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			int i = 0;
-			if (word.trim().length() > 0)
-				pstmt.setString(++i, word);
-			pstmt.setInt(++i, sno);
-			pstmt.setInt(++i, eno);
-
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				MemberDTO dto = new MemberDTO();
-				dto.setId(rs.getString("id"));
-				dto.setMname(rs.getString("mname"));
-				dto.setTel(rs.getString("tel"));
-				dto.setEmail(rs.getString("email"));
-				dto.setZipcode(rs.getString("zipcode"));
-				dto.setAddress1(rs.getString("address1"));
-				dto.setAddress2(rs.getString("address2"));
-				dto.setFname(rs.getString("fname"));
-
-				list.add(dto);
-
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt, rs);
-		}
-		return list;
+		return sqlSession.selectList("member.list", map);
 	}
 
 	public boolean update(MemberDTO dto) {
 		boolean flag = false;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" UPDATE member ");
-		sql.append(" SET tel = ?, email = ?, zipcode = ?, address1 = ?, address2 = ?, job = ? ");
-		sql.append(" WHERE id = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, dto.getTel());
-			pstmt.setString(2, dto.getEmail());
-			pstmt.setString(3, dto.getZipcode());
-			pstmt.setString(4, dto.getAddress1());
-			pstmt.setString(5, dto.getAddress2());
-			pstmt.setString(6, dto.getJob());
-			pstmt.setString(7, dto.getId());
-
-			int cnt = pstmt.executeUpdate();
-			if (cnt > 0) {
-				flag = true;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt);
+		int cnt = sqlSession.update("member.update", dto);
+		if (cnt > 0) {
+			flag = true;
 		}
 		return flag;
 	}
 
 	public boolean updateFile(String id, String fname) {
 		boolean flag = false;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" UPDATE member ");
-		sql.append(" SET fname = ? ");
-		sql.append(" WHERE id = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, fname);
-			pstmt.setString(2, id);
-
-			int cnt = pstmt.executeUpdate();
-			if (cnt > 0) {
-				flag = true;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt);
+		Map map = new HashMap();
+		map.put("id", id);
+		map.put("fname", fname);
+		int cnt = sqlSession.update("member.updateFile", map);
+		if (cnt > 0) {
+			flag = true;
 		}
 		return flag;
 
@@ -213,166 +55,53 @@ public class MemberDAO {
 
 	public boolean updatePw(String id, String passwd) {
 		boolean flag = false;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" UPDATE member ");
-		sql.append(" SET passwd = ? ");
-		sql.append(" WHERE id = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, passwd);
-			pstmt.setString(2, id);
-
-			int cnt = pstmt.executeUpdate();
-			if (cnt > 0) {
-				flag = true;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt);
+		Map map = new HashMap();
+		map.put("id", id);
+		map.put("passwd", passwd);
+		int cnt = sqlSession.update("member.updatePw", map);
+		if (cnt > 0) {
+			flag = true;
 		}
 		return flag;
 	}
 
 	public boolean delete(String id) {
 		boolean flag = false;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" DELETE ");
-		sql.append(" FROM member ");
-		sql.append(" WHERE id = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, id);
-			int cnt = pstmt.executeUpdate();
-			if (cnt > 0) {
-				flag = true;
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt);
+		int cnt = sqlSession.delete("member.delete", id);
+		if (cnt > 0) {
+			flag = true;
 		}
 		return flag;
 
 	}
 
 	public String getFname(String id) {
-		String fname = null;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT fname ");
-		sql.append(" FROM member ");
-		sql.append(" WHERE id = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, id);
-
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				fname = rs.getString("fname");
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt, rs);
-		}
-		return fname;
+		return sqlSession.selectOne("member.getFname", id);
 	}
 
 	public boolean duplicateId(String id) {
 		boolean flag = false;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT COUNT(id) ");
-		sql.append(" FROM member ");
-		sql.append(" WHERE id = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				int cnt = rs.getInt(1);
-				// SELECT COUNT는 숫자니까 int 로 받는다.
-				if (cnt > 0)
-					flag = true; // 중복된 아이디입니다.
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt, rs);
+		int cnt = sqlSession.selectOne("member.duplicateId", id);
+		if (cnt > 0) {
+			flag = true;
 		}
 		return flag;
 	}
 
 	public boolean duplicateEmail(String email) {
 		boolean flag = false;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT COUNT(email) ");
-		sql.append(" FROM member ");
-		sql.append(" WHERE email = ? ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, email);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				int cnt = rs.getInt(1);
-				// SELECT COUNT는 숫자니까 int 로 받는다.
-				if (cnt > 0)
-					flag = true; // 중복된 아이디입니다.
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt, rs);
+		int cnt = sqlSession.selectOne("member.duplicateEmail", email);
+		if (cnt > 0) {
+			flag = true;
 		}
 		return flag;
 	}
 
 	public int total(String col, String word) {
-		int total = 0;
-		Connection con = DBOpen.open();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		StringBuffer sql = new StringBuffer();
-		sql.append(" SELECT COUNT(*) FROM member ");
-		if (word.trim().length() > 0)
-			sql.append(" WHERE " + col + " like '%'||?||'%' ");
-		try {
-			pstmt = con.prepareStatement(sql.toString());
-			if (word.trim().length() > 0)
-				pstmt.setString(1, word);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				total = rs.getInt(1);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			DBClose.close(con, pstmt, rs);
-		}
-		return total;
+		Map map = new HashMap();
+		map.put("col", col);
+		map.put("word", word);
+		return sqlSession.selectOne("member.total", map);
 	}
 
 	public boolean loginCheck(String id, String pw) {
