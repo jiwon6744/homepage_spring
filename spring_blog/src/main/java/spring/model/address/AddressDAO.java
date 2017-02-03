2d1c450610b074e4e4ce47cb74922ed1e369a61c
kimjiwon <skywldnjs21@naver.com>
2017-02-03 12:40:18 +0900

    member mybatis start

--- spring_blog/src/main/java/spring/model/address/AddressDAO.java
+++ spring_blog/src/main/java/spring/model/address/AddressDAO.java
@@ -1,21 +1,12 @@
 package spring.model.address;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import spring.utility.blog.DBClose;
-import spring.utility.blog.DBOpen;
 
 @Repository
 public class AddressDAO {
